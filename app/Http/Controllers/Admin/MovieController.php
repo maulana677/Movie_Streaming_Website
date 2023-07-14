5bf15ca54526cc2942848984943e6ffb938d6e35
maulana677 <maull0776@gmail.com>
2023-07-14 20:17:43 +0700

    edit movie part 2

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class MovieController extends Controller
@@ -70,6 +71,49 @@ class MovieController extends Controller
 
     public function update(Request $request, $id)
     {
+        $data = $request->except('_token');
+
+        $request->validate([
+            'title' => 'required|string',
+            'small_thumbnail' => 'image|mimes:png,jpg,jpeg',
+            'large_thumbnail' => 'image|mimes:png,jpg,jpeg',
+            'trailer' => 'required|url',
+            'movie' => 'required|url',
+            'casts' => 'required|string',
+            'categories' => 'required|string',
+            'release_date' => 'required|string',
+            'about' => 'required|string',
+            'short_about' => 'required|string',
+            'duration' => 'required|string',
+            'featured' => 'required'
+        ]);
+
+        $movie = Movie::find($id);
+
+        if ($request->small_thumbnail) {
+            // save new image
+            $smallThumbnail = $request->small_thumbnail;
+            $originalSmallThumbnailName = Str::random(10).$smallThumbnail->getClientOriginalName();
+            $smallThumbnail->storeAs('public/thumbnail', $originalSmallThumbnailName);
+            $data ['small_thumbnail'] = $originalSmallThumbnailName;
+            
+            // delete old image
+            Storage::delete('public/thumbnail'.$movie->small_thumbnail);
+        }
         
+        if ($request->large_thumbnail) {
+            // save new image
+            $largeThumbnail = $request->large_thumbnail;
+            $originalLargeThumbnailName = Str::random(10).$largeThumbnail->getClientOriginalName();
+            $largeThumbnail->storeAs('public/thumbnail', $originalLargeThumbnailName);
+            $data ['large_thumbnail'] = $originalLargeThumbnailName;
+            
+            // delete old image
+            Storage::delete('public/thumbnail'.$movie->large_thumbnail);
+        }
+
+        $movie->update($data);
+
+        return redirect()->route('admin.movie')->with('success', 'Movie updated successfully');
     }
 }
