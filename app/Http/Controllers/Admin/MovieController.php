c75fe7dc9f73938b293a947e100da24c42dc1ec6
maulana677 <maull0776@gmail.com>
2023-07-14 14:15:41 +0700

    create movie part 1

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -16,4 +16,25 @@ class MovieController extends Controller
     {
         return view('admin.movies-create');
     }
+
+    public function store(Request $request)
+    {
+        $data = $request->except('_token');
+
+        $request->validate([
+            'title' => 'required|string',
+            'small_thumbnail' => 'required|image|mimes:png,jpg,jpeg',
+            'large_thumbnail' => 'required|image|mimes:png,jpg,jpeg',
+            'trailer' => 'required|url',
+            'movie' => 'required|url',
+            'casts' => 'required|string',
+            'categories' => 'required|string',
+            'release_date' => 'required|string',
+            'about' => 'required|string',
+            'short_about' => 'required|string',
+            'duration' => 'required|string',
+            'featured' => 'required'
+        ]);
+        // dd($data);
+    }
 }
