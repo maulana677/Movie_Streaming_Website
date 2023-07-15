98342c01c0944d95eef70d4230522868c26d69cc
maulana677 <maull0776@gmail.com>
2023-07-15 08:39:57 +0700

    admin logout

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -36,4 +36,15 @@ class LoginController extends Controller
         dd($credentials);
 
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+ 
+        $request->session()->invalidate();
+    
+        $request->session()->regenerateToken();
+    
+        return redirect()->route('admin.login');
+    }
 }
