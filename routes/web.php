a38756d0c572b2591b11a1bb1db87a7d83b6de5b
maulana677 <maull0776@gmail.com>
2023-07-17 06:15:43 +0700

    member pricing page

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Member\DashboardController;
 use App\Http\Controllers\Member\RegisterController;
 use App\Http\Controllers\Member\LoginController as MemberLoginController;
 use App\Http\Controllers\Member\MovieController as MemberMovieController;
+use App\Http\Controllers\Member\PricingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,6 +50,8 @@ Route::post('/register', [RegisterController::class, 'store'])->name('member.reg
 //login member
 Route::get('/login', [MemberLoginController::class, 'index'])->name('member.login');
 Route::post('/login', [MemberLoginController::class, 'auth'])->name('member.login.auth');
+
+Route::get('/pricing', [PricingController::class, 'index'])->name('pricing');
 
 //dashboard member
 Route::group(['prefix' => 'member', 'middleware' => ['auth']], function() {
