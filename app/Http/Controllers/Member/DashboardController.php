6b4ed5d3c5d08d9357c4a378315fdc85a78d9f3c
maulana677 <maull0776@gmail.com>
2023-07-16 18:22:34 +0700

    member dashboard part 2

--- app/Http/Controllers/Member/DashboardController.php
+++ app/Http/Controllers/Member/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
-    {
-        return view('member.dashboard');
+    {   
+        $movies = Movie::orderBy('featured', 'DESC')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+
+        return view('member.dashboard', [
+            'movies' => $movies
+        ]);
     }
 }
